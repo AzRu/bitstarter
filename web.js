f6ed0546621ae85c3e6da4ad5b34d09400daf0c5
amir <amir@amir-vm.(none)>
2013-07-10 20:16:05 -0400

    Updated web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 
 var buf = fs.readFileSync('./index.html');
-var txt = buf.toString();
+//var txt = buf.toString();
 
 app.get('/', function(request, response) {
   response.send('txt');
