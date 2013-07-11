c470aaeb3c1ba3b5bd51f7d841b61dac80f1ace0
amir <amir@amir-vm.(none)>
2013-07-10 20:21:46 -0400

    Updated web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 	var fs = require('fs');
 	var txt = fs.readFileSync('index.html', 'utf8');
-	response.send'txt);
+	response.send(txt);
 });
 
 var port = process.env.PORT || 5000;
